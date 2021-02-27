e100ddfa8500c5cd55f0ca247eebb98925fc89e4
Worawatkotc <worawat.kotc@bumail.net>
2021-02-27 16:59:43 +0700

    Link App+FriendList

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import {useState} from 'react';
+import { Link } from "react-router-dom";
+
+
+function App() {
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+  
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () =>{
+    setLines([...lines, text]);
+    setText("");
+  };
+
+  const keyPress = (event) => {
+    if (event.which === 13) {
+      onSend();
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        REACT CHAT
+        <Link to="/friend-list">
+            <button>
+                BACK
+            </button>
+        </Link>
+      </div>
+      <div className="App-chatroom">
+        {
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+              {x}
+              </div>
+          })
+        }
+    </div>
+    <div className="App-textbox">
+      <input type="text" className="App-textbox-Input" 
+      value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+
+      <div className="App-textbox-send" onClick={onSend}>Send</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
